abcd2fb43a824968721be9a0aabd91f755ee8522
jesus m. rodriguez <jesusr@redhat.com>
2020-08-07 12:24:33 -0400

    added Test2

--- main.go
+++ main.go
@@ -75,6 +75,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Test")
 		os.Exit(1)
 	}
+	if err = (&controllers.Test2Reconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Test2"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Test2")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
